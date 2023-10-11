2e77cbc80d8ba512df30802fc8126590fd426aeb
Dell <Dell@DESKTOP-DKNLAOR>
2023-10-11 09:33:55 +0530

    add new

---
+++ com.Equarz.com/src/main/java/com/pagobject/AddandDeletetheaddress_functionality.java
@@ -0,0 +1,97 @@
+package com.pagobject;
+
+public class AddandDeletetheaddress_functionality {
+	package com.pageobjects;
+
+	import org.openqa.selenium.WebDriver;
+	import org.openqa.selenium.WebElement;
+	import org.openqa.selenium.interactions.Actions;
+	import org.openqa.selenium.support.FindBy;
+	import org.openqa.selenium.support.PageFactory;
+
+	import com.Base.Testbase;
+
+	public class BuynowFunctionality extends Testbase{
+		
+		@FindBy(id="si-email")
+		WebElement signin;
+		@FindBy(id="si-password")
+		WebElement pass;
+		@FindBy(xpath="//button[@class='btn btn--primary btn-block btn-shadow']")
+		WebElement   signbttn;
+		
+		@FindBy(css = "[src='http://e-quarz.com/storage/app/public/banner/2023-07-21-64ba5d6aa14b4.png']")
+		WebElement banner;
+		@FindBy (css = "[src='http://e-quarz.com/storage/app/public/product/thumbnail/2023-06-23-64954c5de6998.png']")
+		WebElement SLIMCOAT;
+		@FindBy(css = "[onclick='buy_now()']")
+		WebElement BUYBTN;
+		
+		@FindBy(css ="[alt='Nike']")
+		WebElement nikeproduct;
+		@FindBy(css = "[src='http://e-quarz.com/storage/app/public/product/thumbnail/2023-06-22-64946b6ddb4d7.png']")
+		WebElement nikeshoe;
+		@FindBy(linkText = "//span[text()='Buy now']")
+		WebElement Buybtn;
+		
+		@FindBy(xpath = "(//i[@class='czi-menu align-middle mt-n1 mr-2'])[1]")
+		WebElement categorybtn;
+		@FindBy(linkText = "(//span[text()='Electronics'])[1]")
+		WebElement electronics;
+		@FindBy(linkText = "(//span[text()='Cameras'])[1]")
+		WebElement cameras;
+		@FindBy(xpath = "(//a[@href='http://e-quarz.com/product/fujifilm-x-t5-mirrorless-camera-with-18-55mm-lens-silver-24uZ7t'])[1]")
+		WebElement fujcam;
+		@FindBy(css = "[onclick='buy_now()']")
+		WebElement buysbtn;
+		
+		
+		@FindBy(xpath = "//input[@class='form-control appended-form-control search-bar-input']")
+		WebElement searchbox;
+		@FindBy(xpath ="(//span[@class='input-group-text __text-20px'])[1]")
+		WebElement searchbttn;
+		@FindBy(xpath ="(//a[@href='http://e-quarz.com/product/fujifilm-x-t5-mirrorless-camera-with-18-55mm-lens-silver-24uZ7t'])[1]")
+		WebElement CAMERAproduct;
+		@FindBy(css = "[class='btn btn-secondary element-center __iniline-26 btn-gap-right']")
+		WebElement buybtn;
+		
+		
+		public BuynowFunctionality(WebDriver driver) {
+			PageFactory.initElements(driver, this);
+			
+		}
+		
+		public Hompage bannerproducts() throws Exception {
+			banner.click();
+			SLIMCOAT.click();
+			//Thread.sleep(3000);
+			BUYBTN.click();
+			return new Hompage();
+		}
+		
+		public void homeproducts() {
+			nikeproduct.click();
+			nikeshoe.click();
+			Buybtn.click();
+		}
+		
+		public void categeryproducts() {
+			categorybtn.click();
+			Actions ac=new Actions (driver);
+			ac.moveToElement(electronics).build().perform();
+			cameras.click();
+			fujcam.click();
+			 buysbtn.click();
+		}
+		
+		public void searchproducts() {
+			searchbox.sendKeys(props.getProperty("searchbox"));
+			searchbttn.click();
+			CAMERAproduct.click();
+			buybtn.click();
+		}
+
+	}
+
+
+}
---
+++ com.Equarz.com/src/test/java/com/Equrz/Signup_test.java
@@ -0,0 +1,62 @@
+package com.Equrz;
+
+public class Signup_test {
+	package com.Equarztestcase;
+
+	import org.testng.annotations.AfterMethod;
+	import org.testng.annotations.BeforeMethod;
+	import org.testng.annotations.Test;
+
+	import com.Base.Testbase;
+	import com.pageobjects.BuynowFunctionality;
+	import com.pageobjects.signinFunctionality;
+
+	public class Buypage extends Testbase{
+		
+		
+
+		BuynowFunctionality cf;
+		signinFunctionality sg;
+		public Buypage() {
+			super();
+		
+		}
+		
+		@BeforeMethod
+		public void cart() throws Exception {
+		setup();
+		
+		sg=new signinFunctionality(driver);
+		sg.validatesignin(props.getProperty("Email1"),props.getProperty("Password1"));
+		cf=new BuynowFunctionality(driver);
+		
+		}
+
+		@Test(priority=1)
+		public void banerproducts() throws Exception {
+			cf.bannerproducts();
+			}
+		
+		@Test(priority=2)
+		public void Homproducts() {
+			cf.homeproducts();
+			}
+		@Test(priority=3)
+		public void CategoryProducts() {
+			cf.categeryproducts();
+		}
+	   
+		@Test(priority=4)
+		public void SearchProducts() throws Exception {
+			cf.searchproducts();
+		}
+
+		@AfterMethod
+		public void PRODUCTS() {
+			driver.close();
+		}
+
+	}
+
+
+}
